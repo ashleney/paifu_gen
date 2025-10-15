6c215b1b247b651db703c5872b7b452bc6698e0e
ashlen <thesadru@gmail.com>
2025-10-15 17:47:45 +0200

    add tenhou editing

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 #![allow(dead_code)]
+use std::array::from_fn;
 use std::str::FromStr;
 
 use anyhow::{bail, ensure, Context, Error, Result};
 use riichi::convlog::generate::{generate_mjai_logs, Board, Fuurohai, Sutehai};
-use riichi::convlog::mjai_to_tenhou;
-use riichi::hand::{parse_tile, parse_tiles};
+use riichi::convlog::tenhou::{Log, RawLog};
+use riichi::convlog::{mjai_to_tenhou, tenhou_to_mjai};
+use riichi::hand::{parse_tile, parse_tiles, tiles_to_string};
 use riichi::mjai::Event;
+use riichi::state::PlayerState;
 use riichi::tile::Tile;
+use riichi::tu8;
 use serde::Deserialize;
 use serde::Serialize;
 use serde_json::to_string;
@@ -20,7 +24,7 @@ extern "C" {
     fn log(s: &str);
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct RawBoard {
     pub kyoku: String,
     pub jikaze: String,
@@ -126,6 +130,7 @@ impl TryInto<Board> for RawBoard {
 #[derive(Serialize)]
 struct GenerateResult {
     tenhou_log: String,
+    human_tenhou_log: String,
     mjai_log: Vec<Event>,
     player_id: i32,
 }
@@ -137,18 +142,172 @@ pub fn generate_logs_js(val: JsValue) -> Result<JsValue, JsValue> {
         .try_into()
         .map_err(|e| JsValue::from_str(&format!("parse error: {e}")))?;
     let events = generate_mjai_logs(board).map_err(|e| JsValue::from_str(&format!("log generation error: {e}")))?;
-    let tenhou = mjai_to_tenhou(&events).map_err(|e| JsValue::from_str(&format!("tenhou conversion error: {e}")))?;
-    let string = to_string(&tenhou).map_err(|e| JsValue::from_str(&format!("tenhou serialization error: {e}")))?;
+    let raw_tenhou_log = mjai_to_tenhou(&events).map_err(|e| JsValue::from_str(&format!("tenhou conversion error: {e}")))?;
+    let tenhou_log_string = to_string(&raw_tenhou_log).map_err(|e| JsValue::from_str(&format!("serialization error: {e}")))?;
+    let pretty_tenhou_log = raw_tenhou_log
+        .to_string_pretty()
+        .map_err(|e| JsValue::from_str(&e.to_string()))?;
     let player_id = match events.first() {
         Some(Event::StartGame { id: Some(id), .. }) => *id,
         _ => return Err(JsValue::from_str("mjai logs do not start with StartGame")),
     };
 
     let result = GenerateResult {
-        tenhou_log: string,
+        tenhou_log: tenhou_log_string,
         mjai_log: events,
         player_id: player_id as i32,
+        human_tenhou_log: pretty_tenhou_log,
     };
 
     serde_wasm_bindgen::to_value(&result).map_err(|e| JsValue::from_str(&format!("serialize result error: {e}")))
+}
+
+pub fn tiles_vec_to_string(tiles: &[Tile]) -> String {
+    let mut tiles_34 = [0; 34];
+    let mut aka = [false; 3];
+    for tile in tiles {
+        tiles_34[tile.as_usize()] += 1;
+        match tile.as_u8() {
+            tu8!(5mr) => aka[0] = true,
+            tu8!(5pr) => aka[1] = true,
+            tu8!(5sr) => aka[2] = true,
+            _ => {}
+        }
+    }
+
+    tiles_to_string(&tiles_34, aka).replace(" ", "")
+}
+
+#[wasm_bindgen]
+pub fn generate_board_from_tenhou_js(val: JsValue, jikaze: JsValue) -> Result<JsValue, JsValue> {
+    let raw_tenhou_log: RawLog = from_value(val).map_err(|e| JsValue::from_str(&format!("deserialize error: {e}")))?;
+    let tenhou_log: Log = raw_tenhou_log
+        .try_into()
+        .map_err(|e| JsValue::from_str(&format!("deserialize error: {e}")))?;
+    if tenhou_log.kyokus.is_empty() {
+        return Err(JsValue::from_str("no kyokus"));
+    }
+    let events = tenhou_to_mjai(&tenhou_log).map_err(|e| JsValue::from_str(&format!("parse error: {e}")))?;
+
+    let jikaze_str = jikaze.as_string().ok_or_else(|| JsValue::from_str("invalid jikaze"))?;
+    let jikaze = Tile::from_str(&jikaze_str).map_err(|e| JsValue::from_str(&format!("invalid jikaze: {e}")))?;
+
+    let kyoku = tenhou_log.kyokus[0].meta.kyoku_num;
+    let oya = kyoku % 4;
+    let player_id = (4 + oya + jikaze.as_u8() - tu8!(E)) % 4;
+    // TODO: Do not actually use state to process, use our own
+    let mut state = PlayerState::new(player_id as u8);
+
+    let mut visible_kawa: [Vec<(Tile, bool, bool)>; 4] = from_fn(|_| vec![]);
+    let mut fuuro: [Vec<Vec<(Tile, bool)>>; 4] = from_fn(|_| vec![]);
+    for event in events {
+        state
+            .update(&event)
+            .map_err(|e| JsValue::from_str(&format!("invalid event: {e}")))?;
+        match event {
+            Event::Dahai { actor, pai, tsumogiri } => {
+                visible_kawa[state.rel(actor)].push((pai, tsumogiri, state.riichi_declared[state.rel(actor)]));
+            }
+            Event::Chi {
+                actor,
+                target,
+                pai,
+                consumed,
+            }
+            | Event::Pon {
+                actor,
+                target,
+                pai,
+                consumed,
+            } => {
+                let mut naki = consumed.map(|tile| (tile, false)).to_vec();
+                naki.insert(((4 + actor - target) % 4) as usize - 1, (pai, true));
+                fuuro[state.rel(actor)].push(naki);
+                visible_kawa[state.rel(target)].pop();
+            }
+            Event::Daiminkan {
+                actor,
+                target,
+                pai,
+                consumed,
+            } => {
+                let mut naki = consumed.map(|tile| (tile, false)).to_vec();
+                let t = ((4 + actor - target) % 4) as usize - 1;
+                naki.insert(if t == 2 { 3 } else { t }, (pai, true));
+                visible_kawa[state.rel(target)].pop();
+            }
+            Event::Ankan { actor, consumed } => {
+                let naki = consumed.map(|tile| (tile, false)).to_vec();
+                fuuro[state.rel(actor)].push(naki);
+            }
+            Event::Kakan { actor, pai, .. } => {
+                'outer: for naki in &mut fuuro[state.rel(actor)] {
+                    for (i, (tile, sideways)) in naki.iter().enumerate() {
+                        if tile.deaka() == pai.deaka() && *sideways {
+                            naki.insert(i + 1, (pai, true));
+                            break 'outer;
+                        }
+                    }
+                }
+            }
+            Event::EndKyoku => break,
+            _ => {}
+        }
+    }
+
+    let kawa_strings = visible_kawa
+        .into_iter()
+        .map(|actor_kawa| {
+            actor_kawa
+                .into_iter()
+                .map(|(pai, tsumogiri, riichi)| {
+                    format!(
+                        "{}{}",
+                        tiles_vec_to_string(&[pai]),
+                        if tsumogiri {
+                            "."
+                        } else if riichi {
+                            "-"
+                        } else {
+                            ""
+                        }
+                    )
+                })
+                .collect::<Vec<_>>()
+                .join("")
+        })
+        .collect::<Vec<_>>();
+
+    let fuuro_strings = fuuro
+        .into_iter()
+        .map(|fuuro| {
+            fuuro
+                .into_iter()
+                .rev()
+                .flatten()
+                .map(|(tile, sideways)| {
+                    if sideways {
+                        format!("({})", tiles_vec_to_string(&[tile]))
+                    } else {
+                        tiles_vec_to_string(&[tile]).to_string()
+                    }
+                })
+                .collect::<Vec<_>>()
+                .join("")
+        })
+        .collect::<Vec<_>>();
+
+    let board = RawBoard {
+        kyoku: format!("{}{}", state.bakaze, state.kyoku + 1),
+        jikaze: jikaze_str,
+        kyotaku: state.kyotaku.to_string(),
+        honba: state.honba.to_string(),
+        dora: tiles_vec_to_string(&state.dora_indicators),
+        scores: state.scores.iter().map(|score| score.to_string()).collect::<Vec<_>>(),
+        tehai: tiles_to_string(&state.tehai, state.akas_in_hand).replace(" ", ""),
+        kawa: kawa_strings,
+        fuuro: fuuro_strings,
+    };
+
+    serde_wasm_bindgen::to_value(&board).map_err(|e| JsValue::from_str(&format!("serialize result error: {e}")))
 }
